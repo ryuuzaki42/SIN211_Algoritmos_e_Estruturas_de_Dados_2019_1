05dc117e1b2da37de3df98ab2f2dbcfd44ea9af4
João D. Batista <joao42lbatista@gmail.com>
2020-02-18 11:15:54 -0300

    Update links.r and visualization.jar

---
+++ 1aulas t/cool/links.r
@@ -0,0 +1,18 @@
+## Video aulas
+Linguagem C Descomplicada - prof. André Backes
+https://www.youtube.com/playlist?list=PL8iN9FQ7_jt4DJbeQqv--jpTy-2gTA3Cp
+
+Estrutura de Dados Descomplicada em C - prof. André Backes
+https://www.youtube.com/playlist?list=PL8iN9FQ7_jt6H5m4Gm0H89sybzR9yaaka
+
+Estrutura de Dados em C - UNIVESP
+https://www.youtube.com/playlist?list=PLxI8Can9yAHf8k8LrUePyj0y3lLpigGcl
+
+## Visualization
+https://algorithm-visualizer.org/
+
+https://visualgo.net/
+
+https://www.cs.usfca.edu/~galles/visualization/
+
+    https://www.cs.usfca.edu/~galles/visualization/java/download.html
